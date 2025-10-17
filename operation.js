2208626b9af7147a6de448a0dad02efeb0274d5a
enricoandhika3003-cmd <enrico.andhika3003@gmail.com>
2025-10-17 18:26:32 +0700

    done

---
+++ operation.js
@@ -0,0 +1,29 @@
+function greet() {
+    console.log("Hello");
+}
+
+greet()
+
+const greetMessage = function () {
+    console.log("Good Morning Welcome")
+};
+
+greetMessage()
+
+function addition (a,b) {
+    console.log(a+b)
+}
+
+add(33,34)
+
+function subtract (a,b) {
+    return a-b
+}
+var result = subtract (135,68)
+console.log(result)
+
+function students () {
+    var a=2
+}
+
+console.log(a)
